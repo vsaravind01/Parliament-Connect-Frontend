90e6067f7d36dfd77f2344a4ce83c9057254cb60
vsaravind01 <vsaravind01@gmail.com>
2022-08-28 17:12:41 +0530

    v3.2

--- src/context/auth/authProvider.js
+++
@@ -1,8 +0,0 @@
-import React, { useState } from "react";
-
-export const AuthContext = React.createContext();
-
-export const AuthProvider = ({ children }) => {
-	const [authState, setAuthState] = useState({ name: "", isAuthenticated: false });
-	return <AuthContext.Provider value={{ authState, setAuthState }}>{children}</AuthContext.Provider>;
-};
